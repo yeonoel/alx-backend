c2a0fa768bad1eee1ed3de4d056a4abf8407e928
yeonoel <yeopevroguinoel@gmail.com>
2023-01-31 18:14:38 +0000

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Basic flask app."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    """ list des langues prises en charges"""
+    LANGUAGES = ['en', 'fr']
+    # default config
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ return template """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale from request. """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if '__name__' == '__main__':
+    app.run()
